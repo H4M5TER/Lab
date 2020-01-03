851c79fb6e91a506483634fea2d0b3fda9316465
19264389271 <zhuxx2003@gmail.com>
2020-01-04 05:17:29 +0800

    Update IdPool.h

--- IdPool/IdPool.h
+++ IdPool/IdPool.h
@@ -1,12 +1,7 @@
 #pragma once
 
+// #include <cassert>
 #include <vector>
-
-struct Id
-{
-    int id;
-    Id* next_available; // null == inUse
-};
 
 class IdPool
 {
@@ -15,28 +10,39 @@ public:
         :m_pool_size(pool_size), m_ids(pool_size + 1)
     {
         m_first_available = &m_ids[0];
-        m_ids[pool_size].nextAvailable = NULL;
+        m_ids[pool_size].next_available = NULL;
+        m_ids[pool_size].id = -1;
         for (int i = pool_size - 1; i >= 0; --i)
         {
-            m_ids[i].nextAvailable = &m_ids[i + 1];
+            m_ids[i].next_available = &m_ids[i + 1];
             m_ids[i].id = i;
         }
     }
+
     int Allocate()
     {
-        assert(m_first_available != NULL)
-        int new_id = m_first_available->id;
-        m_first_available = m_first_available->next_available;
-        return new_id;
+        // assert(m_first_available != NULL)
+        Id* new_id = m_first_available;
+        m_first_available = new_id->next_available;
+        new_id->next_available = NULL;
+        return new_id->id;
     }
+
     void Free(int id)
     {
-        assert(m_ids[id]->next_available != NULL)
-        m_ids[id]->next_available = m_first_available;
-        m_first_available->next_available = &m_ids[id];
+        // assert(m_ids[id].next_available != NULL);
+        // assert(m_first_available == NULL);
+        m_ids[id].next_available = m_first_available;
+        m_first_available = &m_ids[id];
     }
-    ~IdPool();
+
+    ~IdPool() {}
 private:
+    struct Id
+    {
+        int id;
+        Id* next_available; // null == inUse
+    };
     const int m_pool_size;
     std::vector<Id> m_ids;
     Id* m_first_available;
